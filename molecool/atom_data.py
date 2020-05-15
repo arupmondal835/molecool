c860f1e4760bc7d3a2ebfb30b93fa9530cfe0f05
arup mondal <arup.mondal@chem.ufl.edu>
2020-05-15 14:42:35 -0400

    group all atom data dictionary

--- molecool/atom_data.py
+++ molecool/atom_data.py
@@ -9,3 +9,15 @@ atomic_weights = {
     'Br': 79.904,
 }
 
+atom_colors = {
+    'H': 'white',
+    'C': '#D3D3D3',
+    'N': '#add8e6',
+    'O': 'red',
+    'P': '#FFA500',
+    'F': '#FFFFE0',
+    'Cl': '#98FB98',
+    'Br': '#F4A460',
+    'S': 'yellow'
+}
+
