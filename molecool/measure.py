4b16a88df862fbce3a53d3b3888bca747aa39f05
arup mondal <arup.mondal@chem.ufl.edu>
2020-05-22 13:38:00 -0400

    ass dockstring

--- molecool/measure.py
+++ molecool/measure.py
@@ -5,9 +5,33 @@ this module is for functions which just calculate something
 
 import numpy as np
 def calculate_distance(rA, rB):
+    """
+    calculate the distance between two points
+    
+    parameters
+    --------------
+    rA, rB : np.ndarray
+        The coordinates of each points
+
+    returns
+    -------
+    distance : float
+        the distance between the two points
+
+    Examples
+    ---------
+    >>> r1=np.array([0.0,0.0,0.0])
+    >>> r2=np.array([0.0,0.0,1.0])
+    >>> calculate_distance(r1,r2)
+    1.0
+    """
     # This function calculates the distance between two points given as numpy arrays.
+    if isinstance(rA, np.ndarray) is False or isinstance(rB, np.ndarray) is False:
+        raise TypeError("rA and rB must be numpy arrays")
     d=(rA-rB)
     dist=np.linalg.norm(d)
+    if dist==0.0:
+        raise Exception("two atoms are at same place")
     return dist
 
 def calculate_angle(rA, rB, rC, degrees=False):
--- molecool/molecule.py
+++ molecool/molecule.py
@@ -1,5 +1,27 @@
-
+from .measure import calculate_distance
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
+    """calculate bonds in a molecule based on a distance criteria.
+
+    the pairwise distance between atoms is computed. If it is in the range 'min_bond' to 'max_bond', the atoms are counted as     bonded.
+
+    Parameters
+    ----------
+    coordinates:np.ndarray
+        the coordinate of the atoms.
+    max_bond : float(optional)
+        the max distance for two atom to be consider bonded. the default is 1.5
+    min_bond: float(optional)
+        the in distance for two point to be considered bonded. the default is 0.
+
+    Returns
+    --------
+    bonds: dict
+        a disctionary where the keys are tuples of bonded atom indices, and the associated values are the bond lengths.
+    
+    """
+
+    if min_bond <0:
+        raise ValueError(F"{min_bond} entered for minimum bond length. minimum bond length can not be less than ZERO!")
 
     # Find the bonds in a molecule (set of coordinates) based on distance criteria.
     bonds = {}
